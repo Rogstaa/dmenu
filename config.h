d343fe2972bfeb3c8e92b97e019553bb9b8a5334
Hoegbo <hoegbo@protonmail.com>
2023-01-17 21:59:59 +0100

    cleaning

--- config.h
+++ config.h
@@ -9,9 +9,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+    [SchemeNorm] = { "#c6c8d1", "#161821" },
+    [SchemeSel] = { "#c6c8d1", "#84a0c6" },
+    [SchemeOut] = { "#c6c8d1", "#a093c7" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
